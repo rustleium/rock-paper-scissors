318653790b978f4fb2204c1a28505aba106a5f7a
Rustam <189756814+rustleium@users.noreply.github.com>
2025-01-08 09:33:01 +0530

    refactor(game): move playRound out of playGame

--- script.js
+++ script.js
@@ -31,26 +31,26 @@ function getHumanChoice() {
 
 // console.log(getHumanChoice())
 
+function playRound(humanChoice, computerChoice) {
+
+  if(humanChoice === computerChoice) {
+    console.log(`You both picked ${computerChoice}. It's a Tie!`);
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    humanScore += 1;
+    console.log(`You picked ${humanChoice}. Computer picked ${computerChoice}. You win.`);
+    
+  } else {
+    computerScore += 1;
+    console.log(`You picked ${humanChoice}. Computer picked ${computerChoice}. Computer wins.`)
+  }
+  console.log(`Human Score: ${humanScore} | Computer Score: ${computerScore}`);
+}
 
 function playGame() {
-  function playRound(humanChoice, computerChoice) {
-  
-    if(humanChoice === computerChoice) {
-      console.log(`You both picked ${computerChoice}. It's a Tie!`);
-    } else if (
-      (humanChoice === "rock" && computerChoice === "scissors") ||
-      (humanChoice === "paper" && computerChoice === "rock") ||
-      (humanChoice === "scissors" && computerChoice === "paper")
-    ) {
-      humanScore += 1;
-      console.log(`You picked ${humanChoice}. Computer picked ${computerChoice}. You win.`);
-      
-    } else {
-      computerScore += 1;
-      console.log(`You picked ${humanChoice}. Computer picked ${computerChoice}. Computer wins.`)
-    }
-    console.log(`Human Score: ${humanScore} | Computer Score: ${computerScore}`);
-  }
   
   playRound(getHumanChoice(), getComputerChoice());
 
